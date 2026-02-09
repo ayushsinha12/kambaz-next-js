fed855ac7a4e9e67bf48ae6aabcc6b901ea3c232
ayushsinha12 <ayushsinha2445@gmail.com>
2026-02-09 00:07:56 -0500

    a2 CSS and Bootstrap

--- app/(kambaz)/courses/[cid]/layout.tsx
+++ app/(kambaz)/courses/[cid]/layout.tsx
@@ -1,12 +1,16 @@
 import type { ReactNode } from "react";
 import CourseNavigation from "./navigation";
 
-export default function CourseLayout({
+export default async function CourseLayout({
   children,
+  params,
 }: {
   children: ReactNode;
-  params?: Promise<{ cid: string }>;
+  params: Promise<{ cid: string }>;
 }) {
+  // You don't have to use cid yet, but this keeps types correct in Next 16
+  await params;
+
   return (
     <div className="row">
       <div className="col-2">
