4520d27bb9a1c86ac33c077f3e038ed65499db01
ayushsinha12 <ayushsinha2445@gmail.com>
2026-02-09 00:03:54 -0500

    a2 CSS and Bootstrap

--- app/(kambaz)/courses/[cid]/layout.tsx
+++ app/(kambaz)/courses/[cid]/layout.tsx
@@ -1,7 +1,12 @@
 import type { ReactNode } from "react";
 import CourseNavigation from "./navigation";
 
-export default function CourseLayout({ children }: { children: ReactNode }) {
+export default function CourseLayout({
+  children,
+}: {
+  children: ReactNode;
+  params?: Promise<{ cid: string }>;
+}) {
   return (
     <div className="row">
       <div className="col-2">
